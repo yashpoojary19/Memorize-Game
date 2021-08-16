fac70963eb595dcf4f92e3e8d864c2048d6c8581
Yash Poojary <yashspoojary245@gmail.com>
2021-08-16 12:01:13 +0530

    Updated label

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Yash Poojary on 16/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Life!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Memorize_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_GameApp.swift
+//  Shared
+//
+//  Created by Yash Poojary on 16/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
